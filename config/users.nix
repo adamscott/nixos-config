0215165d01de96f4749391862466ec29dc416094
Adam Scott <ascott.ca@gmail.com>
2017-06-17 01:14:22 +0000

    Add hashed password (since it's salted)

--- config/users.nix
+++ config/users.nix
@@ -6,5 +6,6 @@
       home = "/home/adam";
       description = "Adam Scott";
       extraGroups = [ "wheel" "networkmanager" ];
+      hashedPassword = "$6$Fq8kOD2XUTuv1cu$WakOdJEUkaIuN8smk6u5S9OxHGHeZ.zpTplaZtiO8FdVHdk2pgEdbAm4IuWxWR6yrg.yr9f3tURbjg9lJaiwL0";
     };
 }
