193d40eccfd5f2145b104a62c184ea0108e0b6ff
adamscott <ascott.ca@gmail.com>
2019-02-03 16:42:29 -0500

    Add hashedPassword

--- common/users/default.nix
+++ common/users/default.nix
@@ -7,5 +7,6 @@
     home = "/home/adam";
     description = "Adam Scott";
     extraGroups = [ "wheel" "networkmanager" ];
+    hashedPassword = "$6$sx.cOyl6MM$RCagTJvR09Dm4JBgRgcpo5fStp7TtOuIBL7QPNQSlZLaB5wQ86g24RhM0xyPlc9K4c0AFdCrumYReoo0AALgK1";
   };
 }
